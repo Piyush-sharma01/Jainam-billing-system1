0f6cdc8ed2a29e461db763c2dfeaa44b212a8f69
piyush sharma <piyushdadhich5445@gmail.com>
2026-08-06 19:49:19 +0530

    Update BillingSystemApplication.java

--- billing-backend/src/main/java/com/jainam/BillingSystemApplication.java
+++ billing-backend/src/main/java/com/jainam/BillingSystemApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -30,5 +31,10 @@ public class BillingSystemApplication {
                     .maxAge(3600);
             }
         };
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
