89b355b5340a6ea4223240a0e9784e5dc2da31c1
piyush sharma <piyushdadhich5445@gmail.com>
2026-08-08 13:02:47 +0530

    Create BrandController.java

---
+++ billing-backend/src/main/java/com/jainam/controller/BrandController.java
@@ -0,0 +1,29 @@
+package com.jainam.controller;
+
+import com.jainam.dto.BrandDTO;
+import com.jainam.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/brands")
+public class BrandController {
+    @Autowired
+    private BrandService brandService;
+
+    @GetMapping
+    public ResponseEntity<List<BrandDTO>> getAllBrands() {
+        return ResponseEntity.ok(brandService.getAllBrands());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createBrand(@RequestBody BrandDTO brandDTO) {
+        try {
+            return ResponseEntity.ok(brandService.createBrand(brandDTO));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
