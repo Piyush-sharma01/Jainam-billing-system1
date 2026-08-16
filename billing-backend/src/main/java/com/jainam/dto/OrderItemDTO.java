8d4e7f7fe7bd1c0c43594169bd5189e9dcbeff47
piyush sharma <piyushdadhich5445@gmail.com>
2026-08-16 18:31:54 +0530

    Create OrderItemDTO.java

---
+++ billing-backend/src/main/java/com/jainam/dto/OrderItemDTO.java
@@ -0,0 +1,22 @@
+package com.jainam.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemDTO {
+    private Long id;
+    private Long productId;
+    private String productName;
+    private String productImageUrl;
+    private Integer quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal gstPercentage;
+    private BigDecimal subtotal;
+    private BigDecimal taxAmount;
+    private BigDecimal total;
+}
